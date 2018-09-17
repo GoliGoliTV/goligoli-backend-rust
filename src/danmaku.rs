8d6c1e1005a4c87022e68bc219449e5a72949094
FourierMeow <41997567+FourierMeow@users.noreply.github.com>
2018-09-17 11:38:35 +0800

    fix: add "hello, world" to danmaku and video

--- src/danmaku.rs
+++ src/danmaku.rs
@@ -2,6 +2,10 @@ use actix_web::{App, HttpRequest};
 
 pub fn config(app: App) -> App {
     app.scope("/danmaku", |proj_scope| {
-        // proj_scope.resource("", |r| r.get().f(user_test))
+        proj_scope.resource("", |r| r.get().f(index))
     })
+}
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hello world!"
 }
