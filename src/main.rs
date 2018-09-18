e992be8fdc70a478e3673410ee8d7816f3e098ff
imoyakin <imoyaki@outlook.com>
2018-09-18 23:02:18 +0800

    add ssl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{server, App, HttpRequest};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 mod danmaku;
 mod user;
@@ -9,13 +10,19 @@ fn index(_req: &HttpRequest) -> &'static str {
 }
 
 fn main() {
+    //load ssl keys
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder.set_private_key_file("key.pem",SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
+
     server::new(|| {
         App::new()
             .resource("/", |r| r.f(index))
             .configure(user::config)
             .configure(video::config)
             .configure(danmaku::config)
-    }).bind("0.0.0.0:8088")
+    }).bind_ssl("127.0.0.1:8088", builder)
     .unwrap()
     .run();
 }
