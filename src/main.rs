05fb57294dd1dc8005bbf82fc6e1419539250dcb
FourierMeow <41997567+FourierMeow@users.noreply.github.com>
2018-09-20 21:28:01 +0800

    refactor(ssl): use dotenv to load ssl files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{server, App, HttpRequest};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use log::trace;
 
 mod danmaku;
 mod user;
@@ -10,12 +11,23 @@ fn index(_req: &HttpRequest) -> &'static str {
 }
 
 fn main() {
+    pretty_env_logger::init();
+
+    dotenv::dotenv().ok();
+    let private_key_file =
+        dotenv::var("PRIVATE_KEY_FILE").expect("PRIVATE_KEY_FILE variable not set in .env file");
+    let cert_file = dotenv::var("CERTIFICATE_CHAIN_FILE")
+        .expect("CERTIFICATE_CHAIN_FILE variable not set in .env file");
+    trace!(".env file loaded");
+
     // load ssl keys
     let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     builder
-        .set_private_key_file("key.pem", SslFiletype::PEM)
-        .unwrap();
-    builder.set_certificate_chain_file("cert.pem").unwrap();
+        .set_private_key_file(private_key_file, SslFiletype::PEM)
+        .expect("Fail to load private key file");
+    builder
+        .set_certificate_chain_file(cert_file)
+        .expect("Fail to load cert file");
 
     server::new(|| {
         App::new()
