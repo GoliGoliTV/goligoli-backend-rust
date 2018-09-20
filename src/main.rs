f957e207603c51cae6aa106e1db23c2fd1c657c4
FourierMeow <41997567+FourierMeow@users.noreply.github.com>
2018-09-20 21:58:36 +0800

    feat: add pretty_logger, logs to stdout instead of stderr

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{server, App, HttpRequest};
+use env_logger::Target;
+use log::trace;
 
 mod danmaku;
 mod user;
@@ -9,6 +11,18 @@ fn index(_req: &HttpRequest) -> &'static str {
 }
 
 fn main() {
+    let mut log_builder = pretty_env_logger::formatted_builder().unwrap();
+    // let's just set some random stuff.. for more see
+    // https://docs.rs/env_logger/0.5.0-rc.1/env_logger/struct.Builder.html
+    log_builder.target(Target::Stdout);
+    if let Ok(s) = ::std::env::var("RUST_LOG") {
+        log_builder.parse(&s);
+    };
+    log_builder.init();
+
+    dotenv::dotenv().ok();
+    trace!(".env file loaded");
+
     server::new(|| {
         App::new()
             .resource("/", |r| r.f(index))
